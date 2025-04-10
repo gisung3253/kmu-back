4e0f2c2ce3771cb3501e175dd4dd53022c9a3f7e
gisung3253 <gisung3253@naver.com>
2025-04-11 02:56:49 +0900

    tset2

--- config/database.js
+++ config/database.js
@@ -6,8 +6,9 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.NODE_ENV === 'production' ? {
-        rejectUnauthorized: true
+    // SSL 설정 수정
+    ssl: process.env.USE_SSL === 'true' ? {
+        rejectUnauthorized: false  // SSL 인증서 검증 비활성화
     } : false
 });
 
